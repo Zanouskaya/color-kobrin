2506d15c8b82323dd398b053eae7269b7dd4c8ec
Aliaksandr Zanouski <aliaksandrzanouski@wsa-050b-e1-pl.local>
2021-12-15 22:58:03 +0100

    fixed color mapping

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
-
 // var Itemdata = document.querySelector('.card');
 
 var items = [
   {
-    category: 'Краска',
-    name: 'Белоснежка',
-    description: 'Матовая краска используется для внутренних работ',
+    id: "p_belosnezhka",
+    type: "paint",
+    category: "Краска",
+    name: "Белоснежка",
+    description: "Матовая краска используется для внутренних работ",
   },
   {
-    category: 'Краска',
-    name: 'Фасадная',
-    description: 'Матовая краска используется для наружних работ',
+    id: "p_fasadnaya",
+    type: "paint",
+    category: "Краска",
+    name: "Фасадная",
+    description: "Матовая краска используется для наружних работ",
   },
   {
-    category: 'Краска',
-    name: 'Фасадная',
-    description: 'Матовая краска используется для наружних работ',
+    id: "p_evrostar",
+    type: "paint",
+    category: "Краска",
+    name: "Евростар",
+    description: "Матовая краска используется для наружних работ",
   },
   {
-    category: 'Шпатлевка',
-    name: '1jhv',
-    description: 'Матовая краска используется для наружних работ',
+    id: "p_np1ps",
+    type: "putty",
+    category: "Шпатлевка",
+    name: "НП 1 ПС",
+    description: "Матовая краска используется для наружних работ",
   },
 ];
 
-
-
-document.getElementById('test').innerHTML = items.map(item =>
-  `<div class='card' id='card'>
-    <div id='tag'><h5>${item.category}</h5></div>
+document.getElementById("test").innerHTML = items
+  .map(
+    (item) =>
+      `<div class='card'>
+    <div id=${item.id}><h5>${item.category}</h5></div>
     <h3>${item.name}</h3>
     <p>${item.description}</p>
     <button>Узнать больше</button>
   </div>`
-).join('');
-
+  )
+  .join("");
 
 items.forEach((item) => {
-    if (item.category === 'Краска') {
-        document.getElementById("tag").style.background = "#fa7f0e"
-    }
-  }, {});
+  if (item.type == "paint") {
+    document.getElementById(item.id).style.background = "#fa7f0e";
+  }
+}, {});
