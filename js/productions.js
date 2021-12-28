a1151c637ac092bb457fff95caab48019d09362b
Zanouskaya <volha_harahliad@icloud.com>
2021-12-28 11:22:30 +0100

    folder-structure

--- js/productions.js
+++ js/productions.js
@@ -2,18 +2,19 @@ import { productsList } from './modules/products_data.js';
 import { filterSelection } from './modules/filterSelection.js';
 
 document.getElementById("test").innerHTML = productsList.map((item) =>
-      `<div class='card ${item.type}' id=${item.id}>
+      `<div class='card ${item.type}' id=${item.id}> <a href=${item.link}>
         <div class='card-top'>
         <div id='${item.id}_title' class='tag'><span>${item.category}</span></div>
         <button><i class="far fa-heart"></i></button>
         </div>
         <div class='card-description'>
-          <img src='./images/belosnezhka.png'>
+          <img src='${item.image}'>
           <h3>${item.name}</h3>
           <p>${item.description}</p>
-        </div>
+        </div></a>
       </div>`
   ).join("");
+
 
 productsList.forEach((item) => {
   if (item.type == "paint") {
@@ -23,7 +24,18 @@ productsList.forEach((item) => {
   } else if (item.type == "putty") {
     document.getElementById(`${item.id}_title`).style.background = "#0055A9";
     document.getElementById(`${item.id}`).style.borderColor = "#0055A9";
-  }
+  } else if (item.type == "primers") {
+    document.getElementById(`${item.id}_title`).style.background = "green";
+    document.getElementById(`${item.id}`).style.borderColor = "green";
+  } else if (item.type == "plasters") {
+    document.getElementById(`${item.id}_title`).style.background = "red";
+    document.getElementById(`${item.id}`).style.borderColor = "red";
+  } else if (item.type == "azure") {
+    document.getElementById(`${item.id}_title`).style.background = "purple";
+    document.getElementById(`${item.id}`).style.borderColor = "purple";
+  } else if (item.type == "glue") {
+    document.getElementById(`${item.id}_title`).style.background = "purple";
+    document.getElementById(`${item.id}`).style.borderColor = "purple";}
 }, {});
 
 filterSelection('card');
