da7a1c27f832ec6cbda92c3d8dea7675f8d345d6
Volha Zanouskaya <volhazanouskaya@wsa-050b-e1-pl.local>
2022-01-07 11:07:53 +0100

    add top line in palitra

--- js/palitra.js
+++ js/palitra.js
@@ -1,9 +1,47 @@
 import { colorsList } from "./modules/colors.js";
 
+// change class name for buttons
+var btnsWrapper = document.getElementById("btnsWrapper");
+
+var btns = btnsWrapper.getElementsByTagName("button");
+// console.log(btns);
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", activeBtn);
+}
+
+function activeBtn() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+}
+
+search = document.getElementById("search");
+search.addEventListener("keyup", filterName);
+
+function filterName() {
+  var input, filter, i, cards;
+  input = document.getElementById("search");
+  cards = document.getElementsByClassName("color-card");
+  filter = input.value.toUpperCase();
+
+  for (i = 0; i < cards.length; i++) {
+    var title = cards[i].getElementsByTagName("h3")[0];
+
+    if (title.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      cards[i].className += " show";
+    } else {
+      while (cards[i].className.includes("show")) {
+        cards[i].className = cards[i].className.replace(" show", "");
+      }
+    }
+  }
+}
+var cards = document.getElementsByClassName("color-card");
+
 document.getElementById("colors").innerHTML = colorsList
   .map(
     (color) =>
-      `<div class='color-card' id='colorCard_${color.id}' > 
+      `<div class='color-card show' id='colorCard_${color.id}' > 
         <div class='color-back' style= "background-color: ${color.rgb_o}">
         </div>
         <h3>${color.name}</h3>
@@ -11,23 +49,18 @@ document.getElementById("colors").innerHTML = colorsList
   )
   .join("");
 
-// var cards = document.getElementById("colors");
-// console.log(cards);
-var cards = document.getElementsByClassName("color-card");
-
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", function () {
-    makeActive(cards[i].id);
+    showAll(cards[i].id);
   });
 }
 
-function makeActive(elemId) {
-  let cards = document.getElementsByClassName("color-card");
+function showAll(elemId) {
   for (let i = 0; i < cards.length; i++) {
-    if (cards[i].id != elemId || cards[i].className.includes("active")) {
-      cards[i].className = cards[i].className.replace(" active", "");
+    if (cards[i].id != elemId || cards[i].className.includes("more-colors")) {
+      cards[i].className = cards[i].className.replace(" more-colors", "");
     } else if (cards[i].id == elemId) {
-      cards[i].className += " active";
+      cards[i].className += " more-colors";
       let re = new RegExp(/\d+/);
       let id = elemId.match(re)[0];
       allColors(id);
@@ -41,37 +74,37 @@ function allColors(id) {
       document.getElementById(
         "all-colors"
       ).innerHTML = `<div class='all-colors-columns'>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_1}">
           </div>
           <h3>1:1</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_2}">
           </div>
           <h3>1:2</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_4}">
           </div>
           <h3>1:4</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_8}">
           </div>
           <h3>1:8</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_16}">
           </div>
           <h3>1:16</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_40}">
           </div>
           <h3>1:40</h3>
       </div>
-      <div class='color-card'>
+      <div class='color-card show'>
           <div class='color-back' style= "background-color: ${item.rgb_80}">
           </div>
           <h3>1:80</h3>
