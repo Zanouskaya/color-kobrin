daca7865d7e8d0686e758038e6929e3aae744617
Zanouskaya <volha_harahliad@icloud.com>
2022-01-26 15:17:23 +0100

    caluclator input function

--- js/calculator.js
+++ js/calculator.js
@@ -1,28 +1,32 @@
-const submitCalculate = document.querySelector(".submitBtn");
-submitCalculate.addEventListener("click", calculator);
+// let inputSurface = document.querySelectorAll("input");
+// console.log(inputSurface);
+// inputSurface.addEventListener('change', calculator);
 
-function calculator(event) {
-  const productsSelect = document.querySelector(".product-select");
-  var rashod = productsSelect.value;
-  var width = parseInt(document.getElementById("width").value);
-  var height = parseInt(document.getElementById("height").value);
-  var square = width * height;
-  var result = square * rashod;
-  event.preventDefault();
-  document.getElementById(
-    "result"
-  ).innerHTML = `<p>площадь помещения: ${square} м2</p>
-      <p>расход краски: ${rashod} г/м2</p>
-       <p>необходимое количество краски: ${result} грамм </p>
-       <p>количество грунтовки</p>`;
-}
+
+// function calculator(event) {
+//   const productsSelect = document.querySelector(".product-select");
+//   var rashod = productsSelect.value;
+//   var width = parseInt(document.getElementById("width").value);
+//   var height = parseInt(document.getElementById("height").value);
+//   var square = width * height;
+//   var result = square * rashod;
+//   event.preventDefault();
+//   document.getElementById(
+//     "result"
+//   ).innerHTML = `<p>площадь помещения: ${square} м2</p>
+//       <p>расход краски: ${rashod} г/м2</p>
+//        <p>необходимое количество краски: ${result} грамм </p>
+//        <p>количество грунтовки</p>`;
+// }
 
 let addSurfaceBtn = document.querySelector(".add-surface");
 
 addSurfaceBtn.addEventListener("click", addSurface);
 
 var clicks = document.getElementById("surfaceAreas").childElementCount;
-function addSurface() {
+function addSurface(e) {
+  e.preventDefault();
+
   clicks += 1;
   document.querySelector(".surface-areas").insertAdjacentHTML(
     "afterbegin",
@@ -39,6 +43,7 @@ function addSurface() {
               min="0" /></label
           ><button class="btn del-surface ${clicks}"><i class="far fa-times-circle"></i></button>
         </div>`
+       
   );
   let surfaceAreas = document.querySelector(".surface-areas");
   let delSurfaceBtn = surfaceAreas.querySelectorAll(".del-surface");
@@ -56,6 +61,29 @@ function addSurface() {
         current[j].remove();
       }
     }
-    // console.log(current);
+    
+  }
+  let inputSurface = document.querySelectorAll("input");
+
+  for (let i=0; i<inputSurface.length; i++) {
+    inputSurface[i].addEventListener('input', calculator);
+    console.log(inputSurface[i])
   }
 }
+
+function calculator(e) {
+  const productsSelect = document.querySelector(".product-select");
+  var rashod = productsSelect.value;
+  var width = parseInt(document.getElementById("width").value);
+  var height = parseInt(document.getElementById("height").value);
+  var square = width * height;
+  var result = square * rashod;
+  e.preventDefault();
+  document.getElementById(
+    "result"
+  ).innerHTML = `<p>площадь помещения: ${square} м2</p>
+      <p>расход краски: ${rashod} г/м2</p>
+       <p>необходимое количество краски: ${result} грамм </p>
+       <p>количество грунтовки</p>`;
+}
+
