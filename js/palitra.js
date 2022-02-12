70d0e71e929f828bdd753c1e0a83dde26b2a29d5
Volha Zanouskaya <volhazanouskaya@wsa-050b-e1-pl.local>
2022-02-12 20:52:19 +0100

    palitra changes

--- js/palitra.js
+++ js/palitra.js
@@ -50,119 +50,237 @@ function showCards(rgb) {
     .map(
       (color) =>
         `<div class='color-card show' id='colorCard_${color.id}' >
+
           <div class='color-back' style= "background-color: ${color[rgb]}">
-          
+            <div class='icon-wrapper'>
+              <button class='palitra-icon'><img src = './images/icons/bookmark.png' style='width: 20px; '></button>
+              <button class='palitra-icon btn-full-screen' id='btn-screen_${color.id}'><img src = './images/icons/crop.png' style='width: 20px;'></button>
+            </div>
           </div>
           
           <h4>${color.name}</h4>
         </div>`
     )
     .join("");
-  showModal();
+  // showModal();
 }
 
 //rendering all-colors-card
-document.getElementById(
-  "modal-title"
-).innerHTML = `Выберите цвет, чтобы увидеть оттенки`;
-function showAll(elemId) {
-  for (let i = 0; i < cards.length; i++) {
+// document.getElementById(
+//   "modal-title"
+// ).innerHTML = `Выберите цвет, чтобы увидеть оттенки`;
+// function showAll(elemId) {
+//   for (let i = 0; i < cards.length; i++) {
+//     let re = new RegExp(/\d+/);
+//     let id = elemId.match(re)[0];
+//     colorsList.forEach((item) => {
+//       if (item.id == id) {
+//         (document.getElementById("modal-title").innerHTML = `
+//         ${item.name}`),
+//           (document.getElementById("allColors").innerHTML = `
+//         <div class='all-colors-columns'>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_1}">
+//               </div>
+//               <h4>1:1</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_2}">
+//               </div>
+//               <h4>1:2</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_4}">
+//               </div>
+//               <h4>1:4</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_8}">
+//               </div>
+//               <h4>1:8</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_16}">
+//               </div>
+//               <h4>1:16</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_40}">
+//               </div>
+//               <h4>1:40</h4>
+//           </div>
+//           <div class='color-card show'>
+//               <div class='color-back' style= "background-color: ${item.rgb_80}">
+//               </div>
+//               <h4>1:80</h4>
+//           </div>
+//           </div>
+//           `);
+//       }
+//     });
+//   }
+// }
+//add event listener for cards => open modal (allColors palitra)
+// function showModal() {
+//   for (let i = 0; i < cards.length; i++) {
+//     cards[i].addEventListener("click", function () {
+//       if (!cards[i].className.includes(" show-modal")) {
+//         showAll(cards[i].id);
+//         cards[i].className += " show-modal";
+//         allColors.style.display = "block";
+//         closeBtn.style.display = "block";
+//         for (let j = 0; j < cards.length; j++) {
+//           if (j != i && cards[j].className.includes(" show-modal")) {
+//             cards[j].className = cards[j].className.replace(" show-modal", "");
+//           }
+//         }
+//       } else if (cards[i].className.includes(" show-modal")) {
+//         cards[i].className = cards[i].className.replace(" show-modal", "");
+//         allColors.style.display = "none";
+//         closeBtn.style.display = "none";
+//       }
+//     });
+//   }
+// }
+
+//close modal by click on top line of modal
+// closeLine.addEventListener("click", function () {
+//   if (allColors.style.display === "none" && closeBtn.style.display === "none") {
+//     allColors.style.display = "block";
+//     closeBtn.style.display = "block";
+//   } else {
+//     allColors.style.display = "none";
+//     closeBtn.style.display = "none";
+//   }
+// });
+//close modal by click not on the modal
+// document.addEventListener("mouseup", function (e) {
+//   var modal = document.getElementById("modalWrapper");
+//   if (!modal.contains(e.target)) {
+//     allColors.style.display = "none";
+//     closeBtn.style.display = "none";
+//     for (let i = 0; i < cards.length; i++) {
+//       if (cards[i].className.includes(" show-modal")) {
+//         cards[i].className = cards[i].className.replace(" show-modal", "");
+//       }
+//     }
+//   }
+// });
+
+let fullScreenBtns = document.getElementsByClassName("btn-full-screen");
+// console.log(fullScreenBtns);
+for (let i = 0; i < fullScreenBtns.length; i++) {
+  fullScreenBtns[i].addEventListener("click", function () {
+    fullScreen(fullScreenBtns[i].id);
+  });
+}
+
+function fullScreen(elemId) {
+  for (let i = 0; i < fullScreenBtns.length; i++) {
     let re = new RegExp(/\d+/);
     let id = elemId.match(re)[0];
     colorsList.forEach((item) => {
       if (item.id == id) {
-        (document.getElementById("modal-title").innerHTML = `
-        ${item.name}`),
-          (document.getElementById("allColors").innerHTML = `
-        <div class='all-colors-columns'>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_1}">
+        let fullscreenDiv = document.getElementById("full-screen");
+        fullscreenDiv.style.display = "flex";
+        fullscreenDiv.innerHTML = `
+        <div style= 'background-color: ${item.rgb_o}; 
+                      width: 100%; 
+                      height: 100%; 
+                      display: flex; 
+                      flex-direction: column; 
+                      justify-content: space-between;'>
+            <div>
+              <h1 style='padding-top: 10px; padding-left: 10px;'>${item.name} </h1>
+              <button class='palitra-icon btn-close-screen' id='btn-close-screen' style='margin: 0px; '><img src = './images/icons/close.png' style='width: 20px; '></button>
+            </div>
+            <div class="all-colors-columns">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_1}"></div>
+                <h4>1:1</h4>
               </div>
-              <h4>1:1</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_2}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_2}"></div>
+                <h4>1:2</h4>
               </div>
-              <h4>1:2</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_4}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_4}"></div>
+                <h4>1:4</h4>
               </div>
-              <h4>1:4</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_8}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_8}"></div>
+                <h4>1:8</h4>
               </div>
-              <h4>1:8</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_16}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_16}"></div>
+                <h4>1:16</h4>
               </div>
-              <h4>1:16</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_40}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_40}"></div>
+                <h4>1:40</h4>
               </div>
-              <h4>1:40</h4>
-          </div>
-          <div class='color-card show'>
-              <div class='color-back' style= "background-color: ${item.rgb_80}">
+              <div class="color-card show">
+                <div class="color-back" style="background-color: ${item.rgb_80}"></div>
+                <h4>1:80</h4>
               </div>
-              <h4>1:80</h4>
-          </div>
-          </div>
-          `);
+            </div>
+        </div>
+        `;
       }
     });
   }
+  closeScreen();
 }
-//add event listener for cards => open modal (allColors palitra)
-function showModal() {
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function () {
-      if (!cards[i].className.includes(" show-modal")) {
-        showAll(cards[i].id);
-        cards[i].className += " show-modal";
-        allColors.style.display = "block";
-        closeBtn.style.display = "block";
-        for (let j = 0; j < cards.length; j++) {
-          if (j != i && cards[j].className.includes(" show-modal")) {
-            cards[j].className = cards[j].className.replace(" show-modal", "");
-          }
-        }
-      } else if (cards[i].className.includes(" show-modal")) {
-        cards[i].className = cards[i].className.replace(" show-modal", "");
-        allColors.style.display = "none";
-        closeBtn.style.display = "none";
-      }
+
+function closeScreen() {
+  let closeScreenBtns = document.getElementsByClassName(
+    "palitra-icon btn-close-screen"
+  );
+  console.log(closeScreenBtns);
+  for (let i = 0; i < closeScreenBtns.length; i++) {
+    closeScreenBtns[i].addEventListener("click", function () {
+      let fullScreenDiv = document.getElementById("full-screen");
+      fullScreenDiv.style.display = "none";
     });
   }
 }
 
-//close modal by click on top line of modal
-closeLine.addEventListener("click", function () {
-  if (allColors.style.display === "none" && closeBtn.style.display === "none") {
-    allColors.style.display = "block";
-    closeBtn.style.display = "block";
-  } else {
-    allColors.style.display = "none";
-    closeBtn.style.display = "none";
-  }
-});
-//close modal by click not on the modal
-document.addEventListener("mouseup", function (e) {
-  var modal = document.getElementById("modalWrapper");
-  if (!modal.contains(e.target)) {
-    allColors.style.display = "none";
-    closeBtn.style.display = "none";
-    for (let i = 0; i < cards.length; i++) {
-      if (cards[i].className.includes(" show-modal")) {
-        cards[i].className = cards[i].className.replace(" show-modal", "");
-      }
-    }
-  }
-});
+// document.addEventListener("mouseup", function (e) {
+//   var fullScreen = document.getElementById("full-screen");
+//   if (!fullScreen.contains(e.target)) {
+//     fullScreen.style.display = "none";
+//   }
+// });
 
-document.getElementById("btn-full").addEventListener("click", () => {
-  document.getElementById("full-screen").style.display = "block";
-});
+// {
+//   <div class="all-colors-columns">
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_1}"></div>
+//       <h4>1:1</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_2}"></div>
+//       <h4>1:2</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_4}"></div>
+//       <h4>1:4</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_8}"></div>
+//       <h4>1:8</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_16}"></div>
+//       <h4>1:16</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_40}"></div>
+//       <h4>1:40</h4>
+//     </div>
+//     <div class="color-card show">
+//       <div class="color-back" style="background-color: ${item.rgb_80}"></div>
+//       <h4>1:80</h4>
+//     </div>
+//   </div>;
+// }
