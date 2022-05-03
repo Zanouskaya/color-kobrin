555aef3540fd6127e2d56a54b0182ab3836f4822
Zanouskaya <volha_harahliad@icloud.com>
2022-05-03 22:16:48 +0200

    updated app.js

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`dirname: ${__dirname}`);
+  console.log(`color-kobrin app listening on port ${port}`);
+});
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
 });
